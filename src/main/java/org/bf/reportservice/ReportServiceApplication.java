71cb86a9b393b28d49c93f78d2388afede83584e
Yejin-source <yejin3831@naver.com>
2025-12-02 11:28:35 +0900

    initial commit

---
+++ src/main/java/org/bf/reportservice/ReportServiceApplication.java
@@ -0,0 +1,13 @@
+package org.bf.reportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportServiceApplication.class, args);
+	}
+
+}
