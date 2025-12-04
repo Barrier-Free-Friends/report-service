8458f7815bd8f1d5761ff04148548ae4f8174e97
Yejin-source <yejin3831@naver.com>
2025-12-04 15:54:58 +0900

    add: 서브모듈 추가

--- src/main/java/org/bf/reportservice/ReportServiceApplication.java
+++ src/main/java/org/bf/reportservice/ReportServiceApplication.java
@@ -2,8 +2,15 @@ package org.bf.reportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ComponentScan(basePackages = {
+        "org.bf.reportservice",
+        "org.bf.global"
+})
 public class ReportServiceApplication {
 
 	public static void main(String[] args) {
